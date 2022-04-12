0b58e419f19e07e873cd087e830b4825edc0293d
zillur-rgb <zillurdeu@gmail.com>
2022-04-12 03:46:52 +0200

    Fetch data from firebase firestore

---
+++ src/pages/Category.jsx
@@ -0,0 +1,85 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import {
+  collection,
+  where,
+  query,
+  orderBy,
+  limit,
+  getDocs,
+} from "firebase/firestore";
+import { db } from "../firebase.init";
+import { toast } from "react-toastify";
+import Spinner from "../Components/Spinner/Spinner";
+import ListingItem from "../Components/ListingItem/ListingItem";
+
+const Category = () => {
+  const [listings, setListings] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const params = useParams();
+
+  useEffect(() => {
+    const fetchListing = async () => {
+      try {
+        //Get Reference
+        const listingRef = collection(db, "listings");
+
+        // Create a query
+        const q = query(
+          listingRef,
+          where("type", "==", params.categoryName),
+          orderBy("timestamp", "desc"),
+          limit(10)
+        );
+
+        //Execute query
+        const querySnap = await getDocs(q);
+
+        //Running a loop to get data
+        const listing = [];
+
+        querySnap.forEach((doc) => {
+          return listing.push({
+            id: doc.id,
+            data: doc.data(),
+          });
+        });
+        setListings(listing);
+        setLoading(false);
+      } catch (error) {
+        toast.error("Could not fetch data");
+      }
+    };
+    fetchListing();
+  }, []);
+
+  return (
+    <div>
+      <header>
+        <p className="pageHeader">
+          {params.categoryName === "rent"
+            ? "Places for Rent"
+            : "Places for Sale"}
+        </p>
+
+        {loading ? (
+          <Spinner />
+        ) : listings && listings.length > 0 ? (
+          listings.map((listing) => {
+            return (
+              <ListingItem
+                key={listing.id}
+                id={listing.id}
+                listingData={listing.data}
+              />
+            );
+          })
+        ) : (
+          <p>No listings available for {params.categoryName}</p>
+        )}
+      </header>
+    </div>
+  );
+};
+
+export default Category;
