bd35ddf8205f637289e82f097ab4a588efc2f083
zillur-rgb <zillurdeu@gmail.com>
2022-05-15 23:13:49 +0200

    checking

--- components/Navbar/MenuToggle.jsx
+++ components/Navbar/MenuToggle.jsx
@@ -1,16 +1,16 @@
 import { RiMenuLine } from "react-icons/ri";
 import { FaTimes } from "react-icons/fa";
-import { Box } from "@chakra-ui/react";
+import { Box, Flex } from "@chakra-ui/react";
 
 const MenuToggle = ({ handleSetClick, clicked }) => {
   return (
-    <Box display={{ base: "block", md: "none" }} onClick={handleSetClick}>
+    <Flex display={{ base: "block", md: "none" }} onClick={handleSetClick}>
       {!clicked ? (
         <RiMenuLine fontSize={28} fontWeight="medium" />
       ) : (
         <FaTimes fontSize={28} fontWeight="medium" />
       )}
-    </Box>
+    </Flex>
   );
 };
 
